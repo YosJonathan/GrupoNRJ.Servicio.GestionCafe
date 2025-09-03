8549e3b952b719684b8107638ac14a95b979487c
Jonathan Estuardo Yos Escobar <78168255+YosJonathan@users.noreply.github.com>
2025-09-02 20:32:23 -0600

    [jyos][Patron Singleton]

---
+++ GrupoNRJ.Servicio.GestionCafe/GrupoNRJ.Servicio.GestionCafe/Controllers/InventarioController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GrupoNRJ.Servicio.GestionCafe.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class InventarioController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Obtener()
+        {
+            var inventario = GestorDeInventario.Instancia.ObtenerInventario();
+            return Ok(inventario);
+        }
+
+        [HttpPost("agregar")]
+        public IActionResult AgregarProducto(int idProducto, int cantidad)
+        {
+            GestorDeInventario.Instancia.AgregarProducto(idProducto, cantidad);
+            return Ok($"Se agregaron {cantidad} unidades del producto {idProducto}");
+        }
+
+        [HttpPost("retirar")]
+        public IActionResult RetirarProducto(int idProducto, int cantidad)
+        {
+            GestorDeInventario.Instancia.RetirarProducto(idProducto, cantidad);
+            return Ok($"Se retiraron {cantidad} unidades del producto {idProducto}");
+        }
+    }
+}
